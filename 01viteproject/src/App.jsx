867d808e9c7ee3f255f1aa260341efa12ace9c8d
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-04-06 14:44:50 +0530

    props functality with the help of tailwind css and devUi and tailwindcss

---
+++ 01viteproject/src/App.jsx
@@ -0,0 +1,18 @@
+
+function App() {
+  let counter = 5
+
+  return (
+    <>
+     <h1> Chai aur react </h1>
+     <h2> Counter value: {counter}</h2>
+
+     <button>Add value</button>
+     <br />
+     <button>Remove value</button>
+    </>
+   
+  )
+}
+
+export default App
