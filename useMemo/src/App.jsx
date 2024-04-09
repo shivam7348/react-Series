9b55c83f7dcd39e8a6d6e581156f7602d19c10b1
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-04-09 14:47:05 +0530

    const multi = useMemo(it does take function and depencies in the array form )

--- useMemo/src/App.jsx
+++ useMemo/src/App.jsx
@@ -1,7 +1,26 @@
+import { useMemo } from "react"
+import { useState } from "react"
+
   function App() {
+    const [add, setadd] = useState(0)
+    const [minus, setminus] = useState(100)
+
+    const multiplication = useMemo(function multiply() {
+      console.log("****&*");
+      return add *10
+    }, [add])
+
+   
     return (
       <>
       <h1> Learning UseMemo</h1>
+      { multiplication} <br />
+      <button onClick={() => setadd(add + 1)} >AddValue</button>
+      <span>{add}</span>
+      <br />
+      <br />
+      <button onClick={() => setminus(minus - 1)}>SubValue</button>
+      <span>{minus}</span>
       </>
     )
   }
