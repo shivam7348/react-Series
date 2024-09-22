f019e27c330ecad9d9007f6cbf2532e04a31bb8d
Shivam Kumar <106791774+shivam7348@users.noreply.github.com>
2024-09-21 19:19:57 -0700

    setTimeOutr is a function

---
+++ Asynchronous code/app.js
@@ -0,0 +1,17 @@
+// console.log("one");
+// console.log("two");
+// console.log("three");
+// console.log("four");
+// console.log("five");
+
+// function hello () {
+//     console.log("hello");
+    
+// }
+// setTimeout(() => {
+//     hello, 2000) // timeout 
+
+setTimeout(() => {
+    console.log("hello");
+    
+}, 4000)  // setTimeout is a function where we can passa function and time in millisecond
